a2a3f36da67552d795d6f9cb1975923e4741db8c
Aoife McDaid <aoife.mcdaid@gmail.com>
2017-12-03 22:41:37 +0100

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171129223709) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tour_dates", force: :cascade do |t|
     t.datetime "date_time"
     t.string   "venue"
