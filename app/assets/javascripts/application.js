cc2cba86d0963a63923c0e5139612951e5597080
Aoife McDaid <aoife.mcdaid@gmail.com>
2018-02-04 19:31:57 +0100

    Update css and image

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,17 +21,30 @@
 //= require_tree .
 
 
-var thresholdPx = 145;
-$(window).scroll(function() {
-  var scrollTop = $(document).scrollTop();
-
-  if (scrollTop > thresholdPx) {
-    $('.navbar-container').css({
-      background: 'rgba(253,192,47,0.7)'
+// var thresholdPx = 145;
+$(() => {
+  $(window).scroll(function() {
+    const navbarHeight = $('.navbar-container').height();
+    // console.log('navbarHeight', navbarHeight);
+    const $sections = $('.section');
+    $sections.each((i, section) => {
+      const {y} = section.getBoundingClientRect();
+      const sectionPosition = y + navbarHeight;
+      // console.log('sectionPosition', sectionPosition);
+      const pageHeight = $(window).height();
+      // console.log('pageHeight', pageHeight);
+      if (sectionPosition > 0 && sectionPosition < (pageHeight / 3)) {
+        console.log(section.id);
+        // debugger;
+        console.log('selector', `#${section.id}-link`);
+        $(`.navbar-links a:not(#${section.id}-link`).each((i, link) => {
+          $(link).removeClass('active');
+        })
+        // $('.section')
+        $(`#${section.id}-link`).addClass('active');
+        return false;
+      }
     })
-  } else if (scrollTop <= thresholdPx) {
-    $('.navbar-container').css({
-      background: 'transparent'
-    })
-  }
-});
+  });
+  
+})
